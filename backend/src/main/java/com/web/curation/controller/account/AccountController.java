81df732ddf1f2fe9b043f09257e8a79c271c23e7
SBHbro <ssd018@gmail.com>
2020-07-21 17:56:52 +0900

    기능 : 로그인 백엔드 기능 추가 이메일과 비밀번호를 받았을때 검사하여 토큰을 반환해줌 #S03P12B302-113

--- backend/src/main/java/com/web/curation/controller/account/AccountController.java
+++ backend/src/main/java/com/web/curation/controller/account/AccountController.java
@@ -1,5 +1,9 @@
 package com.web.curation.controller.account;
 
+import static org.junit.Assert.assertEquals;
+
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.util.Optional;
 
 import javax.validation.Valid;
@@ -20,7 +24,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import io.fusionauth.jwt.Signer;
+import io.fusionauth.jwt.Verifier;
+import io.fusionauth.jwt.domain.JWT;
 import io.fusionauth.jwt.hmac.HMACSigner;
+import io.fusionauth.jwt.hmac.HMACVerifier;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -43,17 +50,16 @@ public class AccountController {
             @RequestParam(required = true) final String password) {
 
         Optional<User> userOpt = userDao.findUserByUserEmailAndUserPwd(email, password);
-
         ResponseEntity response = null;
-        System.out.println(userOpt);
         if (userOpt.isPresent()) {
             final BasicResponse result = new BasicResponse();
             result.status = true;
             result.data = "success";
 //            String token = getToken(userOpt);
-            response = new ResponseEntity<>(result, HttpStatus.OK);
+            String token = getToken(email, password);
+            response = new ResponseEntity<>(token, HttpStatus.OK);
         } else {
-            response = new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+            response = new ResponseEntity<>("Fail", HttpStatus.NOT_FOUND);
         }
 
         return response;
@@ -74,18 +80,30 @@ public class AccountController {
     }
     
  // 암호화 하는 방법임 : 내이름 넣음
- 	static Signer signer = HMACSigner.newSHA256Signer("hong jeong min");
+ 	static Signer signer = HMACSigner.newSHA256Signer("park se hun");
 
-// 	static public String getToken(Member member) {
-// 		// Userid로 토큰을 만든다.
-// 		// plusMinutes 는 토큰을 등록하는 시간임 지금은 1분
-// 		JWT jwt = new JWT().setIssuer(member.getUserid()).setIssuedAt(ZonedDateTime.now(ZoneOffset.UTC))
-// 				.setSubject("f1e33ab3-027f-47c5-bb07-8dd8ab37a2d3")
-// 				.setExpiration(ZonedDateTime.now(ZoneOffset.UTC).plusMinutes(1));
-// 		// Sign and encode the JWT to a JSON string representation
-// 		String token = JWT.getEncoder().encode(jwt, signer);
-//
-// 		return token;
-// 	}
+ 	static public String getToken(String email, String password) {
+ 		// Userid로 토큰을 만든다.
+ 		// plusMinutes 는 토큰을 등록하는 시간임 지금은 1분
+ 		JWT jwt = new JWT().setIssuer(email).setIssuedAt(ZonedDateTime.now(ZoneOffset.UTC))
+ 				.setSubject("f1e33ab3-027f-47c5-bb07-8dd8ab37a2d3")
+ 				.setExpiration(ZonedDateTime.now(ZoneOffset.UTC).plusMinutes(1));
+ 		// Sign and encode the JWT to a JSON string representation
+ 		String token = JWT.getEncoder().encode(jwt, signer);
+ 		return token;
+ 	}
 
+ // 복호화 하는 방법 : 내이름 넣음 
+ 	// 토큰이 필요한 API 정보에 대해서 유효성을 체크해주면된다
+ 	static Verifier verifier = HMACVerifier.newVerifier("park se hun");
+ 	static public boolean cmpToken(String token) {
+ 		try {
+ 			// Build an HMC verifier using the same secret that was used to sign the JWT
+ 			JWT jwt = JWT.getDecoder().decode(token, verifier);
+ 			assertEquals(jwt.subject, "f1e33ab3-027f-47c5-bb07-8dd8ab37a2d3");
+ 		} catch (Exception e) {
+ 			return false;
+ 		}
+ 		return true;
+ 	}
 }

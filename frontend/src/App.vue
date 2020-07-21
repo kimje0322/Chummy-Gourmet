cb55a02d1a725726c802cd798b13b7bffb7ce466
phy <dig03089@naver.com>
2020-07-21 16:26:25 +0900

    기능 : 로그인 페이지 추가 #S03P12B302-115

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,57 +1,52 @@
 <template>
   <div id="app">
     <v-app>
-  <v-main>
-  <v-card
-    class="overflow-hidden mx-auto"
-    height="667"
-    max-width="500"
-  >
-   <v-app-bar
-      color="deep-blue accent-4"
-      dense
-      dark
-    >
-      <v-toolbar-title style="margin:auto;">돈독한 미식가</v-toolbar-title>
-      
-    </v-app-bar>
-    <v-bottom-navigation
-      scroll-target="#scroll-area-2"
-      hide-on-scroll
-      scroll-threshold="500"
-      absolute
-      color="white"
-      horizontal
-    >
-      <v-btn text color="deep-purple accent-4">
-        <v-icon>fas fa-search</v-icon>
-      </v-btn>
-      <v-btn text color="deep-purple accent-4">
-        <v-icon>fas fa-plus</v-icon>
-      </v-btn>
+      <v-main>
+        <v-card class="overflow-hidden mx-auto" height="667" max-width="375">
+          <v-app-bar color="deep-blue accent-4" dense dark>
+            <v-toolbar-title style="margin:auto;">돈독한 미식가</v-toolbar-title>
+          </v-app-bar>
+          <v-bottom-navigation
+            scroll-target="#scroll-area-2"
+            hide-on-scroll
+            scroll-threshold="500"
+            absolute
+            color="white"
+            horizontal
+          >
+            <v-btn text color="deep-purple accent-4">
+              <router-link to="/map">
+                <v-icon>fas fa-search</v-icon>
+              </router-link>
+            </v-btn>
 
-      <v-btn text color="deep-purple accent-4">
-        <v-icon>fas fa-list</v-icon>
-      </v-btn>
+            <v-btn text color="deep-purple accent-4">
+              <router-link to="/party">
+                <v-icon>fas fa-plus</v-icon>
+              </router-link>
+            </v-btn>
 
-      <v-btn text color="deep-purple accent-4">
-        <v-icon>fas fa-user</v-icon>
-      </v-btn>
-    </v-bottom-navigation>
+            <v-btn text color="deep-purple accent-4">
+              <router-link to="/feed/main">
+              <v-icon>fas fa-list</v-icon>
+              </router-link>
+            </v-btn>
 
-    <v-sheet
-      id="scroll-area-1"
-      class="overflow-y-auto"
-      max-height="600"
-    >
-      <v-container style="height: 100%;">
-         <router-view></router-view>
-      </v-container>
-    </v-sheet>
-  </v-card>
-  </v-main>
-</v-app>
-   
+            <v-btn text color="deep-purple accent-4">
+              <router-link to="/user/info">
+                <v-icon>fas fa-user</v-icon>
+              </router-link>
+            </v-btn>
+          </v-bottom-navigation>
+
+          <v-sheet id="scroll-area-1" class="overflow-y-auto" max-height="600">
+            <v-container style="height: 100%;">
+              <router-view></router-view>
+            </v-container>
+          </v-sheet>
+        </v-card>
+      </v-main>
+    </v-app>
   </div>
 </template>
 <script>
--- frontend/src/views/user/Login.vue
+++ frontend/src/views/user/Login.vue
@@ -1,75 +1,89 @@
 <template>
-<v-app id="app">
-  <div class="img-info">
-    <!-- <img src="../../assets/images/logo_ex.png" alt="log" width="100"> -->
-  </div>
-  <!-- <router-view></router-view> -->
+  <v-app id="app">
+    <div class="img-info">
+      <!-- <img src="../../assets/images/logo_ex.png" alt="log" width="100"> -->
+    </div>
+    <!-- <router-view></router-view> -->
 
-  <div class="user" id="login">
-    <div class="wrapC">
-      <!-- <h1>
+    <div class="user" id="login">
+      <div class="wrapC">
+        <!-- <h1>
         돈독한
         <br />미식가
-      </h1> -->
+        </h1>-->
 
-    <div class="input-with-label">
-      <input
-        v-model="email"
-        v-bind:class="{error : error.email, complete:!error.email&&email.length!==0}"
-        @keyup.enter="Login"
-        id="email"
-        placeholder="이메일을 입력하세요."
-        type="text"
-      />
-      <label for="email">이메일</label>
-      <div class="error-text" v-if="error.email">{{error.email}}</div>
-    </div>
+        <div class="input-with-label">
+          <input
+            v-model="email"
+            v-bind:class="{error : error.email, complete:!error.email&&email.length!==0}"
+            @keyup.enter="Login"
+            id="email"
+            placeholder="이메일을 입력하세요."
+            type="text"
+          />
+          <label for="email">이메일</label>
+          <div class="error-text" v-if="error.email">{{error.email}}</div>
+        </div>
 
-    <div class="input-with-label">
-      <input
-        v-model="password"
-        type="password"
-        v-bind:class="{error : error.password, complete:!error.password&&password.length!==0}"
-        id="password"
-        @keyup.enter="Login"
-        placeholder="비밀번호를 입력하세요."
-      />
-      <label for="password">비밀번호</label>
-      <div class="error-text" v-if="error.password">{{error.password}}</div>
-    </div>
+        <div class="input-with-label">
+          <input
+            v-model="password"
+            type="password"
+            v-bind:class="{error : error.password, complete:!error.password&&password.length!==0}"
+            id="password"
+            @keyup.enter="Login"
+            placeholder="비밀번호를 입력하세요."
+          />
+          <label for="password">비밀번호</label>
+          <div class="error-text" v-if="error.password">{{error.password}}</div>
+        </div>
 
-    <v-btn color="warning" width="100%">로그인</v-btn>
+        <v-btn color="warning" width="100%">로그인</v-btn>
 
-      <!-- <v-btn color="info" class="btn btn--back btn--login"
+        <!-- <v-btn color="info" class="btn btn--back btn--login"
       @click="onLogin"
       :disabled="!isSubmit"
       :class="{disabled : !isSubmit}"
-      >로그인</v-btn>  -->
+        >로그인</v-btn>-->
 
-    <div class="sns-login">
-      <div class="text">
-        <p>SNS 간편 로그인</p>
-        <div class="bar"></div>
-      </div>
-      <kakaoLogin :component="component" />
-      <GoogleLogin :component="component" />
-    </div>
-    <div class="add-option">
-      <div class="text">
+        <div class="sns-login">
+          <div class="text">
+            <p>SNS 간편 로그인</p>
+            <div class="bar"></div>
+          </div>
+          <kakaoLogin :component="component" />
+          <GoogleLogin :component="component" />
+        </div>
+        <div class="add-option">
+          <!-- <div class="text">
         <p>혹시</p>
         <div class="bar"></div>
-      </div>
-      <div class="bibun">
-        <router-link to="/user/findpw" class="btn--text"><p>비밀번호를 잊으셨나요?</p></router-link>
-      </div>
-      <div class="wrap">
+          </div>-->
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <div class="row">
+            <p class="d-flex mx-auto">
+              <router-link to="/user/join">
+                <img src="../../assets/images/join_logo.png" alt="join_log" width="15" />
+                회원가입 |
+              </router-link>
+              <router-link to="/user/findpw">
+                <img src="../../assets/images/find_logo.png" alt="find_logo" width="15" />
+                계정찾기
+              </router-link>
+            </p>
+          </div>
+          <!-- <div class="wrap">
         <p>아직 회원이 아니신가요?</p>
         <router-link to="/user/join" class="btn-join"><strong>가입하기!</strong></router-link>
+          </div>-->
+        </div>
       </div>
     </div>
-    </div>
-  </div>
-</v-app>
+  </v-app>
 </template>
 
 <script>
@@ -173,18 +187,20 @@ export default {
 
 
 <style scoped>
+.login-entire {
+  padding: 0;
+}
 
-  .login-entire {
-    padding: 0;
-  }
+.img-info {
+  /* position: absolute; */
+  margin: 15px auto;
+}
 
-  .img-info {
-    /* position: absolute; */
-    margin: 15px auto;
-  }
+.bibun {
+  text-align: center;
+}
 
-  .bibun {
-    float: left;
-  }
-
+.container {
+  height: 50%;
+}
 </style>

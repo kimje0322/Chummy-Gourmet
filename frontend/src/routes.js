7325763fa48ca57b7be6b0293a75f1bf1c9ebe5c
조민기 <hodduco_o@naver.com>
2020-07-28 14:02:45 +0900

    기능 :

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,3 +1,6 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router';
+
 import Login from './views/user/Login.vue'
 import Join from './views/user/Join.vue'
 import JoinRule from './views/user/JoinRule.vue'
@@ -16,104 +19,100 @@ import Map from './views/map/map.vue'
 import MapParty from './views/map/party.vue'
 import Home from './views/user/Home.vue'
 
-
-export default [
-
-
-    {
-        path: '/login',
-        name: 'Login',
-        component: Login,
-        meta: {
-            navbar: true
+Vue.use(VueRouter);
+export default new VueRouter({
+    routes : [
+        {
+            path: '/login',
+            name: 'Login',
+            component: Login,
+            meta: {
+                navbar: true
+            }
+        },
+        {
+            path: '/user/join',
+            name: 'Join',
+            component: Join
+        },
+        {
+            path: '/user/info',
+            name: 'UserInfo',
+            component: UserInfo
+        },
+        {
+            path: '/user/joinrule',
+            name: 'Joinrule',
+            component: JoinRule
+        },
+        {
+            path: '/user/joininfo',
+            name: 'JoinInfo',
+            component: JoinInfo
+        },
+        {
+            path: '/user/findPw',
+            name: 'FindPw',
+            component: FindPw
+        },
+        {
+            path: '/user/finishjoin',
+            name: 'FinshJoin',
+            component: FinishJoin
+        },
+        {
+            path: '/user/pref',
+            name: 'Pref',
+            component: Pref
+        },
+        {
+            path: '/map',
+            name: 'Map',
+            component: Map
+        },
+        {
+            path: '/map/party',
+            name: 'MapParty',
+            component: MapParty
+        },
+        {
+            path: '/feed/main',
+            name: 'FeedMain',
+            component: FeedMain
+        },
+        {
+            path: '/components',
+            name: 'Components',
+            component: Components
+        },
+        {
+            path: '/curator',
+            name: 'Curator',
+            component: Curator
+        },
+        {
+            path: '/detail',
+            name: 'Detail',
+            component: Detail
+        },
+        {
+            path: '/reviewDetail',
+            name: 'ReviewDetail',
+            component: ReviewDetail
+        },
+        {
+            path: '/party',
+            name: 'Party',
+            component: Party
+        },
+        {
+            path: '/',
+            name: 'Home',
+            component: Home,
+            meta: {
+                navbar: true
+            }
         }
-    },
-    {
-        path: '/user/join',
-        name: 'Join',
-        component: Join
-    },
-    {
-        path: '/user/info',
-        name: 'UserInfo',
-        component: UserInfo
-    },
-    {
-        path: '/user/joinrule',
-        name: 'Joinrule',
-        component: JoinRule
-    },
-    {
-        path: '/user/joininfo',
-        name: 'JoinInfo',
-        component: JoinInfo
-    },
-    {
-        path: '/user/findPw',
-        name: 'FindPw',
-        component: FindPw
-    },
-    {
-        path : '/user/finishjoin',
-        name : 'FinshJoin',
-        component : FinishJoin
-    },
-    {
-        path : '/user/findPw',
-        name : 'FindPw',
-        component : FindPw
-    },
-    {
-        path : '/user/pref',
-        name : 'Pref',
-        component : Pref
-    },
-    {
-        path: '/map',
-        name: 'Map',
-        component: Map
-    },
-    {
-        path: '/map/party',
-        name: 'MapParty',
-        component: MapParty
-    },
-    {
-        path: '/feed/main',
-        name: 'FeedMain',
-        component: FeedMain
-    },
-    {
-        path: '/components',
-        name: 'Components',
-        component: Components
-    },
-    {
-        path: '/curator',
-        name: 'Curator',
-        component: Curator
-    },
-    {
-        path: '/detail',
-        name: 'Detail',
-        component: Detail
-    },
-    {
-        path: '/reviewDetail',
-        name: 'ReviewDetail',
-        component: ReviewDetail
-    },
-    {
-        path: '/party',
-        name: 'Party',
-        component: Party
-    },
-    {
-        path: '/',
-        name: 'Home',
-        component: Home,
-        meta: {
-            navbar: true
-        }
-    }
-]
+
+    ]
+});

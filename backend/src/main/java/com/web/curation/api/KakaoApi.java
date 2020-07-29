c2be896632ec1021e787fece6248e17a22a22d10
SBHbro <ssd018@gmail.com>
2020-07-29 19:13:42 +0900

    기능 : 큐레이션 DB설계 API도출 및 구현 큐레이션 DB설게, API도출, 카카오API사용하여 키워드로 음식점의 이름가져오기 까지완료 #S03P12B302-129 #S03P12B302-130 #S03P12B302-131

---
+++ backend/src/main/java/com/web/curation/api/KakaoApi.java
@@ -0,0 +1,115 @@
+package com.web.curation.api;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
+
+public class KakaoApi {
+
+    public static void main(String location) {
+        String clientId = "dapi.kakao.com"; //애플리케이션 클라이언트 아이디값"
+        String clientSecret = "KakaoAK e4cd88afa207146436293dbd18d2b89f"; //애플리케이션 클라이언트 시크릿값"
+
+        String text = new String();
+        try {
+            text = URLEncoder.encode(location, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException("검색어 인코딩 실패",e);
+        }
+
+        String apiURL = " http://dapi.kakao.com/v2/local/search/keyword.json?query="+text+"&category_group_code=FD6";    // json 결과
+
+        Map<String, String> requestHeaders = new HashMap<>();
+        requestHeaders.put("Host", clientId);
+        requestHeaders.put("Authorization", clientSecret);
+        String responseBody = get(apiURL,requestHeaders);
+
+        System.out.println(responseBody);
+        
+        String name = "";
+        //json으로 받아온 값을 식당이름/분류로 나눔
+        try {
+        	JSONParser jsonParse = new JSONParser();
+			JSONObject jsonObj = (JSONObject) jsonParse.parse(responseBody);
+			
+			JSONArray store =(JSONArray) jsonObj.get("documents");
+			System.out.println(store.size());
+			for(int i = 0 ; i<store.size();i++) {
+				JSONObject temp =(JSONObject) store.get(i);
+				StringTokenizer st = new StringTokenizer((String) temp.get("category_name"),">");
+				st.nextToken();
+				System.out.println(temp.get("place_name"));
+				name += temp.get("place_name");
+				if(!(i==store.size()-1))name += ",";
+				System.out.println(st.nextToken().trim());
+				System.out.println(temp.get("road_address_name"));
+				System.out.println("----------------------------");
+			}
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        System.out.println(name);
+    }
+
+    private static String get(String apiUrl, Map<String, String> requestHeaders){
+        HttpURLConnection con = connect(apiUrl);
+        try {
+            con.setRequestMethod("GET");
+            for(Map.Entry<String, String> header :requestHeaders.entrySet()) {
+                con.setRequestProperty(header.getKey(), header.getValue());
+            }
+
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 호출
+                return readBody(con.getInputStream());
+            } else { // 에러 발생
+                return readBody(con.getErrorStream());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("API 요청과 응답 실패", e);
+        } finally {
+            con.disconnect();
+        }
+    }
+
+    private static HttpURLConnection connect(String apiUrl){
+        try {
+            URL url = new URL(apiUrl);
+            return (HttpURLConnection)url.openConnection();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("API URL이 잘못되었습니다. : " + apiUrl, e);
+        } catch (IOException e) {
+            throw new RuntimeException("연결이 실패했습니다. : " + apiUrl, e);
+        }
+    }
+
+    private static String readBody(InputStream body){
+        InputStreamReader streamReader = new InputStreamReader(body);
+
+        try (BufferedReader lineReader = new BufferedReader(streamReader)) {
+            StringBuilder responseBody = new StringBuilder();
+
+            String line;
+            while ((line = lineReader.readLine()) != null) {
+                responseBody.append(line);
+            }
+
+            return responseBody.toString();
+        } catch (IOException e) {
+            throw new RuntimeException("API 응답을 읽는데 실패했습니다.", e);
+        }
+    }
+}
--- frontend/src/views/curator/curator.vue
+++ frontend/src/views/curator/curator.vue
@@ -1,26 +1,42 @@
 <template>
 <div style="padding : 0">
-<input type="text" placeholder="원하는 지역 검색" style="width : 90%; border : 1px solid">
-        <button class="fas fa-search" style="width : 10%" />
-        <br>
-        <p>대전광역시 봉명동에 대한 결과</p>
-        <feed-item />
-        <feed-item2 />
-        <feed-item3 />
+  <input v-model="location" type="text" placeholder="원하는 지역 검색" style="width : 100%; border : 1px solid">
+
+  <v-btn width="100%" @click="search">검색</v-btn>
+  <p>{{location}}에 대한 결과</p>
+       
         
 </div>
 </template>
 <script>
-import FeedItem from "../../components/feed/FeedItem.vue";
-import FeedItem2 from "../../components/feed/FeedItem2.vue";
-import FeedItem3 from "../../components/feed/FeedItem3.vue";
+import axios from "axios";
+
+// const SERVER_URL = "http://i3b302.p.ssafy.io:8080";
+const SERVER_URL = "http://localhost:8080";
 
 export default {
    data () {
       return {
-        activeBtn: 1,
+        location : ""
       }
     },
-  components: { FeedItem,FeedItem2,FeedItem3 },
+    methods :{
+      search(){
+         axios
+        .get(
+          `${SERVER_URL}/curation?location=${this.location}`
+        )
+
+        .then((response) => {
+         alert("성공");
+        })
+
+        .catch((error) => {
+          console.log(error.response);
+          alert("로그인 실패");
+        });
+      }
+    },
+  components: { }
 };
 </script>

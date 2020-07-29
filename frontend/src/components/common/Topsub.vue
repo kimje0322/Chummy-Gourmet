dbb5dea6704b0425c8b8b22ebdf70bbf7a8b2584
happyday751 <happyday751@naver.com>
2020-07-29 17:38:27 +0900

    기능 : 유저페이지 수정 백엔드 연결 S03P12B302-39

--- frontend/src/components/common/Topsub.vue
+++ frontend/src/components/common/Topsub.vue
@@ -4,21 +4,21 @@
       <v-list-item-avatar>
         <v-img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR_doKnSS8nyn0SYPV-J4cQgaE7uHtbsKlB9A&usqp=CAU"></v-img>
       </v-list-item-avatar>
-        <p class="my-auto">돈독이</p>
+      {{proptoTopsub.userName}}
       <v-spacer></v-spacer>
-        <!-- <v-btn depressed>8.8<br>평점</v-btn> -->
+        <v-btn depressed>{{proptoTopsub.followingCount}} <br>팔로잉</v-btn>
       <v-spacer></v-spacer>
-        <v-btn depressed>127 <br>팔로워</v-btn>
-      <!-- <v-spacer></v-spacer> -->
-        <v-btn depressed>42<br>팔로잉</v-btn>
-      <!-- <v-spacer></v-spacer> -->
+        <v-btn depressed>{{proptoTopsub.followerCount}}<br>팔로워</v-btn>
+      <v-spacer></v-spacer>
     </v-toolbar>
   </v-layout>
 </template>
-
-<style scoped>
-  p {
-    /* font-family: 'Jua', sans-serif; */
-    font-size: 17px;
- }
-</style>
+<script>
+export default {
+  props:{
+    proptoTopsub: {
+      type : Object
+    },
+  }
+}
+</script>

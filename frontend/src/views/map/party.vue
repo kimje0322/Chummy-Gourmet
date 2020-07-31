333106e3ecffaa8d4cc69d459324941753fb99b4
SBHbro <ssd018@gmail.com>
2020-07-31 12:46:15 +0900

    핫픽스 : 발표할 때 필요한 부분 추가

--- frontend/src/views/map/party.vue
+++ frontend/src/views/map/party.vue
@@ -1,84 +1,225 @@
 <template>
-  <v-layout row>
-    <v-flex xs12 sm6 offset-sm3>
-      <v-card>
-        <v-img
-          src="https://img.siksinhot.com/place/1485274468095571.jpg?w=307&h=300&c=Y"
-          height="250px"
-        />
+  <div class="user join">
+    <v-bottom-navigation
+      v-if="$route.name === 'Party'"
+      scroll-target="#scroll-area-2"
+      hide-on-scroll
+      scroll-threshold="500"
+      absolute
+      color="white"
+      horizontal
+    >
+      <v-btn-toggle v-model="text" tile color="deep-purple accent-3" group>
+        <!-- <v-col cols="6"> -->
+          <v-btn @click="$router.go(-1)" value="center">취소</v-btn>
+        <!-- </v-col> -->
 
-        <v-card-title>
-          <div>
-            <span class="headline">고기땡기노 </span>
-            <span class="subtitle-1">맛찬들</span>
-          </div>
-        </v-card-title>
-        <v-card-text>
-          <div class="subtitle-1">
-            <v-icon>mdi-map-marker</v-icon>  대전 유성구 온천북로13번길 35
-          </div>
-          <br />
-          <div class="subtitle-1">
-            <v-icon>mdi-calendar-clock</v-icon>  2020-07-18 17:00
-          </div>
-          <br />
-          <div class="subtitle-1">
-            <v-icon>mdi-account-multiple</v-icon>  2명 / 4명
-          </div>
-          <br />
-        </v-card-text>
-          <div align="center">
-            <v-btn color="primary">
-              참석
-            </v-btn>
-            <v-btn href="/#/map" color="error">
-              닫기
-            </v-btn>
-          </div>
-        <v-card-actions>
+        <!-- <v-col cols="6"> -->
+          <v-btn value="right" @click="meetUp">생성</v-btn>
+        <!-- </v-col> -->
+      </v-btn-toggle>
+    </v-bottom-navigation>
+    <v-app>
+      <v-toolbar-title>
+        <v-toolbar dark>
+          <a @click="$router.go(-1)">
+            <i class="fas fa-chevron-left back"></i>
+          </a>
           <v-spacer></v-spacer>
-          <!-- <v-btn icon @click="show = !show">
-            <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-          </v-btn> -->
-        </v-card-actions>
+          <v-spacer></v-spacer>
+          <p class="my-auto">Meet Up</p>
+          <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
+        </v-toolbar>
+      </v-toolbar-title>
+      <br />
 
-        <!-- <v-slide-y-transition>
-          <v-card-text
-            v-show="show"
-          >I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.</v-card-text>
-        </v-slide-y-transition> -->
-      </v-card>
-    </v-flex>
-  </v-layout>
+      <div class="party wrapC">
+        <div class="input-group mb-3">
+          <v-text-field v-model="title" solo label="제목"></v-text-field>
+        </div>
+
+        <div class="input-group mb-3">
+          <v-textarea v-model="content" solo name="input-7-4" label="일정 내용을 입력하세요."></v-textarea>
+        </div>
+
+        <v-form>
+          <!-- <v-row> -->
+          <v-text-field
+            solo
+            label="장소"
+            :append-icon="isColor ? 'mdi-map-marker orange--text text--lighten-2' : 'mdi-map-marker'"
+            @click:append="isColor = !isColor"
+            @click:input="isClick = !isClick"
+          ></v-text-field>
+          <!-- </v-row> -->
+        </v-form>
+
+        <!-- <v-row> -->
+        <v-menu
+          ref="menu"
+          v-model="menu"
+          :close-on-content-click="false"
+          :return-value.sync="date"
+          transition="scale-transition"
+          offset-y
+          min-width="300px"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-text-field
+              solo
+              v-model="date"
+              label="날짜"
+              :append-icon="isColor1 ? 'mdi-calendar-check orange--text text--lighten-2' : 'mdi-calendar-check'"
+              @click:append="isColor1 = !isColor1"
+              readonly
+              v-bind="attrs"
+              v-on="on"
+            ></v-text-field>
+          </template>
+          <v-date-picker
+            :min="new Date().toISOString().substr(0, 10)"
+            max="2050-01-01"
+            v-model="date"
+            no-title
+            scrollable
+          >
+            <v-spacer></v-spacer>
+            <v-btn text color="primary" @click="menu = false">Cancel</v-btn>
+            <v-btn text color="primary" @click="$refs.menu.save(date)">OK</v-btn>
+          </v-date-picker>
+        </v-menu>
+        <!-- </v-row> -->
+
+        <!-- dropdown -->
+        <div id="dropdown-example">
+          <v-overflow-btn
+            v-model="count"
+            solo
+            class="my-2"
+            :items="dropdown_font"
+            label="인원"
+            style="margin:0px;"
+            target="#dropdown-example"
+          ></v-overflow-btn>
+        </div>
+      </div>
+    </v-app>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+
+// const SERVER_URL = "http://i3b302.p.ssafy.io:8080";
+const SERVER_URL = "http://localhost:8080";
+
 export default {
   data: () => ({
-    show: false,
-    items: [
-      { header: "Today" },
-      {
-        avatar: "https://cdn.vuetifyjs.com/images/lists/1.jpg",
-        title: "Brunch this weekend?",
-        subtitle:
-          "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?"
-      },
-      { divider: true, inset: true },
-      {
-        avatar: "https://cdn.vuetifyjs.com/images/lists/2.jpg",
-        title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
-        subtitle:
-          "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend."
-      },
-      { divider: true, inset: true },
-      {
-        avatar: "https://cdn.vuetifyjs.com/images/lists/3.jpg",
-        title: "Oui oui",
-        subtitle:
-          "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
+    // date: new Date().toISOString().substr(0, 10),
+    date: null,
+    menu: false,
+    isColor: false,
+    isColor1: false,
+    isClick: false,
+
+    dropdown_font: ["1", "2", "3", "4"],
+  }),
+
+  mounted() {
+    if (window.kakao && window.kakao.maps) {
+      this.initMap();
+    } else {
+      const script = document.createElement("script");
+      /* global kakao */
+      script.onload = () => kakao.maps.load(this.initMap);
+      script.src =
+        "http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=722f0c506c2743378fac318284106578";
+      document.head.appendChild(script);
+    }
+  },
+  methods: {
+    meetUp() {
+      if (this.title.length === 0) {
+        alert("제목을 작성해주세요.");
+        return;
       }
-    ]
-  })
+
+      if (this.content.length === 0) {
+        alert("내용을 작성해주세요.");
+        return;
+      }
+
+      if (this.date.length === 0) {
+        alert("날짜를 선택해주세요.");
+        return;
+      }
+
+      if (this.count.length === 0) {
+        alert("인원을 선택해주세요.");
+        return;
+      }
+      console.log(this.title);
+      console.log(this.title.length);
+      console.log(this.content);
+      console.log(this.content.length);
+      console.log(this.date);
+      console.log(this.date.length);
+      console.log(this.count);
+      console.log(this.count.length);
+
+      // axios
+      //   .get(
+      //     `${SERVER_URL}/party?title=${this.title}&content=${this.content}`
+      //   )
+    },
+    initMap() {
+      var container = document.getElementById("map");
+      var options = {
+        center: new kakao.maps.LatLng(33.450701, 126.570667),
+        level: 3,
+      };
+
+      var map = new kakao.maps.Map(container, options);
+      // map.setMapTypeId(kakao.maps.MapTypeId.HYBRID);
+    },
+  },
 };
 </script>
+
+<style>
+.back {
+  color: white !important;
+}
+.nothome {
+  padding: 0px !important;
+}
+.party {
+  padding: 0px 12px;
+}
+#map {
+  width: 100px;
+  height: 100px;
+  z-index: 10;
+}
+.section .article {
+  position: relative;
+  color: black;
+}
+div {
+  display: block;
+}
+.tit {
+  position: absolute;
+  top: 8px;
+  left: 0;
+  font-weight: bold;
+}
+ .fa-chevron-left {
+    color: white; 
+    margin-left: 7px;
+  }
+/* .btn-person{
+  margin: 0px;
+} */
+</style>

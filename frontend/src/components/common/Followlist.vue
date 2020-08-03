f485779adc443f7bf4700aebc9b9c4761e0eb0ab
happyday751 <happyday751@naver.com>
2020-08-03 18:18:43 +0900

    기능 : 팔로워/팔로잉 유저 검색바 추가 S03P13B302-44

--- frontend/src/components/common/Followlist.vue
+++ frontend/src/components/common/Followlist.vue
@@ -9,16 +9,31 @@
       <!-- tab view -->
       <v-tabs dark v-model="currentItem" fixed-tabs slider-color="grey">
       <v-tab class="follow-list" v-for="item in items" :key="item" :href="'#tab-' + item">
-        <v-icon v-if="item=='follower'">팔로워</v-icon>
-        <v-icon v-if="item=='following'">팔로잉</v-icon>
+        <v-icon class="followListBtn" v-if="item=='follower'">팔로워</v-icon>
+        <v-icon class="followListBtn" v-if="item=='following'">팔로잉</v-icon>
       </v-tab>
     </v-tabs>
 
     <v-tabs-items v-model="currentItem">
       <v-tab-item v-for="item in items" :key="item" :value="'tab-' + item">
         <v-card flat>
+         <!-- 검색바 -->
+          <v-row>
+            <v-col class="search-bar" cols="12" sm="12">
+              <v-text-field 
+                @keyup.up="onSearch"
+                v-model="searchName"
+                label="검색"
+                solo
+              ></v-text-field>
+              <!-- axios요청 보낼때 searchName -->
+              <!-- <p>{{ searchName }}</p> -->
+            </v-col>
+          </v-row>
           <!-- 팔로워 -->
           <v-list nav dense  v-if="item=='follower'">
+           
+
                 <v-list-item-group v-model="followerList">
                     <v-list-item v-for="(user, i) in followerList" :key="i">
                         <!-- <v-list-item-avatar>
@@ -41,7 +56,7 @@
                     </v-list-item>
                 </v-list-item-group>
             </v-list>
-
+          
             <!-- 팔로잉 -->
             <v-list nav dense v-else>
                 <v-list-item-group v-model="followingList">
@@ -84,6 +99,7 @@ export default {
  
   data: () => {
     return {
+        searchName: "",
         userId: "",
         currentItem: 'tab-Web',
         items: [
@@ -133,11 +149,28 @@ export default {
         console.log(error.response);
       });     
   },
+  methods: {
+    onSearch () {
+      // console.log(value)
+    }
+  }
 };
 </script>
 
 <style scoped>
   .follow-lsit {
     width: 100px
+  }
+
+  .search-bar {
+    padding: 0 12px 0 12px;
+    margin: 10px 18px 0 18px;
+  }
+
+  .followListBtn {
+    font-style: normal;
+    font-size: 15px !important;
+    font-family: 'Jua', sans-serif;
+    
   }
 </style>

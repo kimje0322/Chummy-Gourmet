fa031c7017d9bb40bd42818c4205cc85d4649eb7
SBHbro <ssd018@gmail.com>
2020-08-04 17:05:32 +0900

    기능 : 이미지 업로드 테스트

--- backend/src/main/java/com/web/curation/controller/post/PostController.java
+++ backend/src/main/java/com/web/curation/controller/post/PostController.java
@@ -1,18 +1,23 @@
 package com.web.curation.controller.post;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.web.curation.dao.post.PostDao;
 import com.web.curation.model.BasicResponse;
@@ -35,6 +40,14 @@ public class PostController {
 	@Autowired
 	PostDao postDao;
 
+	@PostMapping("/post/test")
+	public void test(@RequestParam("file") MultipartFile file) throws IOException {
+		System.out.println("파일 이름 : " + file.getOriginalFilename());
+	    System.out.println("파일 크기 : " + file.getSize());
+	    
+	    FileCopyUtils.copy(file.getBytes(), new File("/home/ubuntu/deploy/dist/img/feed/test.img"));
+	}
+	
 	@GetMapping("/post")
 	@ApiOperation(value = "게시글 반환")
 	public Map<String, Object> view(final String userid){

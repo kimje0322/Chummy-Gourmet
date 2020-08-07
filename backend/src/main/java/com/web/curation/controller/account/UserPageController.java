be2e5c1bcdd37480916e94cf498a30cbd3155303
김승범 <tmdwkd2@naver.com>
2020-08-07 12:24:46 +0900

    기능 : 프로필 이미지 불러오기 수정중
    # S03P13B302-99

--- backend/src/main/java/com/web/curation/controller/account/UserPageController.java
+++ backend/src/main/java/com/web/curation/controller/account/UserPageController.java
@@ -146,6 +146,8 @@ public class UserPageController {
 					map.put("followingNickname", user.get().getUserNickname());
 					map.put("followingPhone", user.get().getUserPhone());
 					map.put("followingComment", user.get().getUserComment());
+					map.put("followingImg", user.get().getUserImg());
+					
 					userList.add(map);
 				}
 			}
@@ -203,6 +205,8 @@ public class UserPageController {
 						map.put("followerNickname", user.get().getUserNickname());
 						map.put("followerPhone", user.get().getUserPhone());
 						map.put("followerComment", user.get().getUserComment());
+						map.put("followerImg", user.get().getUserImg());
+						
 						int ans1 = userPageDao.getFollowingCountByUserIdByUserFollowing(userId, followeruserId);
 						int ans2 = userPageDao.getFollowingRequestCountByUserIdByUserFollowing(userId, followeruserId);
 						

3da9661db56c6de69a7fdcef29deb2083d8d7832
SBHbro <ssd018@gmail.com>
2020-08-09 02:55:49 +0900

    기능 : 큐레이터 탭 음식점 사진 추가

--- frontend/src/views/curator/curator.vue
+++ frontend/src/views/curator/curator.vue
@@ -94,8 +94,8 @@ import axios from "axios";
 import router from "@/routes";
 
 
-// const SERVER_URL = "https://i3b302.p.ssafy.io:8080";
-const SERVER_URL = "https://localhost:8080";
+const SERVER_URL = "https://i3b302.p.ssafy.io:8080";
+// const SERVER_URL = "https://localhost:8080";
 
 export default {
   data() {

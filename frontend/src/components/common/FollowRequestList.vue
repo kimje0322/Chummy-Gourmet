4b49a9299af191dac70a9a63b52848585ca378fe
김승범 <tmdwkd2@naver.com>
2020-08-19 18:02:39 +0900

    기능 : 마이페이지 관련된 페이지들 상단바 통일 작업중

--- frontend/src/components/common/FollowRequestList.vue
+++ frontend/src/components/common/FollowRequestList.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-toolbar-title >
-      <v-toolbar dense>
+      <v-toolbar class="mb-1" dense elevation="1">
       <v-icon @click="$router.go(-1)">
         mdi-arrow-left
       </v-icon>

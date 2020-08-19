692c1fd487f853a42616267f4fb15f35d7cee496
SBHbro <ssd018@gmail.com>
2020-08-19 16:16:53 +0900

    기능 : 알림센터 하단 네비게이션 바로 이동, 알림유저 프로필사진 추가

--- frontend/src/views/user/AlarmPage.vue
+++ frontend/src/views/user/AlarmPage.vue
@@ -32,8 +32,8 @@
 import moment from 'moment';
 
 import axios from "axios";
-// const SERVER_URL = "https://i3b302.p.ssafy.io:8080";
-const SERVER_URL = "https://localhost:8080";
+const SERVER_URL = "https://i3b302.p.ssafy.io:8080";
+// const SERVER_URL = "https://localhost:8080";
 
 export default {
   data(){
